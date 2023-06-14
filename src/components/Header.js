efa921e9a428cb69991bc5d44cff398aa5f8b752
zohaib javed <zohaib.javed@invozone.com>
2023-06-14 10:07:16 +0500

    fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,40 +34,60 @@ const Header = () => {
 
   const [name, setName] = useState();
   const [fullName, setFullName] = useState();
+  const [lastName, setLastName] = useState();
+  const [lastNameNew, setLastNameNew] = useState();
 
   const userName = (event) => {
     console.log(event.target.value);
     setName(event.target.value);
   };
 
-  const onSubmit = () => {
+  const onSubmits = (event) => {
+    event.preventDafult();
     setFullName(name);
+    setLastNameNew(lastName);
   };
+
+
+  const userName2 = (event) => {
+    setLastName(event.target.value)
+
+  }
 
   return (
     <div class="login-container">
       <div class="login-box">
-        <h2>Login {fullName}</h2>
-        <form>
+        <h2>Login {fullName} {lastNameNew}</h2>
+        <form onSubmit={onSubmits}>
           <div class="form-group">
             <input
               type="text"
               id="username"
               name="username"
-              placeholder="USERNAME"
+              placeholder="First Name"
               onChange={userName}
               value={name}
             />
           </div>
           <div class="form-group">
             <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Last Name"
+              onChange={userName2}
+              value={lastName}
+            />
+          </div>
+          {/* <div class="form-group">
+            <input
               type="password"
               id="password"
               name="password"
               placeholder="PASSWORD"
             />
-          </div>
-          <button type="submit" onClick={onSubmit}>
+          </div> */}
+          <button type="submit" >
             Submit
           </button>
           <div class="register">
